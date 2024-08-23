63bf4382d618d0aaced60ab32c90a452e17a39bc
Naser Abdullah Alam <40887297+naserAlam@users.noreply.github.com>
2024-08-24 03:57:00 +0600

    output in subfolder

--- utils/pdf_to_image.py
+++ utils/pdf_to_image.py
@@ -2,8 +2,8 @@ import fitz
 import os
 
 # Configuration
-UPLOAD_FOLDER = 'uploads'
-OUTPUT_FOLDER = 'converted_images'
+UPLOAD_FOLDER = '../uploads'
+OUTPUT_FOLDER = '../converted_images'
 ALLOWED_EXTENSIONS = {'pdf'}
 
 # Ensure the upload and output folders exist
@@ -14,7 +14,7 @@ os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 SAMPLE_PDF = '../uploads/System Desgin Handbook.pdf'
 OUTPUT_PATH = '../converted_images/test/'
 
-def pdf_to_image(pdf_path, output_folder, image_format='png'):
+def pdf_to_image(pdf_path, output_folder, file_name, image_format='png'):
     """
     docstring
     """
@@ -22,14 +22,18 @@ def pdf_to_image(pdf_path, output_folder, image_format='png'):
     total_pages = len(pdf_document)
     image_paths = []
 
+    file_name_without_ext = os.path.splitext(file_name)[0]
+    subfolder_path = os.path.join(output_folder, file_name_without_ext)
+    os.makedirs(subfolder_path, exist_ok=True)
+
     for page_num in range(total_pages):
         page = pdf_document[page_num]
         pix = page.get_pixmap()
-        output_image_path = os.path.join(output_folder, f'page_{page_num + 1}.{image_format}')
+        output_image_path = os.path.join(subfolder_path, f'page_{page_num + 1}.{image_format}')
         pix.save(output_image_path)
         image_paths.append(output_image_path)
 
     return image_paths
 
 
-print(pdf_to_image(pdf_path=SAMPLE_PDF, output_folder=OUTPUT_PATH))
+print(pdf_to_image(pdf_path=SAMPLE_PDF, output_folder=OUTPUT_PATH, file_name='test'))
