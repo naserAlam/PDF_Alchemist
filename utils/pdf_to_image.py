e6bdad83e4de5aa3f9c664a44d4eed50ae5394ab
Naser Abdullah Alam <40887297+naserAlam@users.noreply.github.com>
2024-08-24 03:41:43 +0600

    return image paths

--- utils/pdf_to_image.py
+++ utils/pdf_to_image.py
@@ -1,4 +1,14 @@
 import fitz
+import os
+
+# Configuration
+UPLOAD_FOLDER = 'uploads'
+OUTPUT_FOLDER = 'converted_images'
+ALLOWED_EXTENSIONS = {'pdf'}
+
+# Ensure the upload and output folders exist
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
 ### Sample data
 SAMPLE_PDF = '../uploads/System Desgin Handbook.pdf'
@@ -9,12 +19,17 @@ def pdf_to_image(pdf_path, output_folder, image_format='png'):
     docstring
     """
     pdf_document = fitz.open(pdf_path)
-    for page_num in range(len(pdf_document)):
+    total_pages = len(pdf_document)
+    image_paths = []
+
+    for page_num in range(total_pages):
         page = pdf_document[page_num]
         pix = page.get_pixmap()
-        output_image_path = f'{output_folder}/page_{page_num + 1}.{image_format}'
+        output_image_path = os.path.join(output_folder, f'page_{page_num + 1}.{image_format}')
         pix.save(output_image_path)
-    
-    print('Conversion Complete')
+        image_paths.append(output_image_path)
 
-pdf_to_image(pdf_path=SAMPLE_PDF, output_folder=OUTPUT_PATH)
+    return image_paths
+
+
+print(pdf_to_image(pdf_path=SAMPLE_PDF, output_folder=OUTPUT_PATH))
